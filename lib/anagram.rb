f67c4aa96d09e4064d06a60e7b211899613c9a37
Nicolas Marini <nmarini44@gmail.com>
2018-06-18 21:51:48 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,14 +1,14 @@
 # Your code goes here!
 class Anagram
-attr_accessor :word 
+attr_accessor :word
   def initialize(word)
     @word = word
   end
 
   def match(array)
     array.select{|w| w.split(//).sort == @word.split(//).sort }
-    
+
   end
-  
+
 end
   
